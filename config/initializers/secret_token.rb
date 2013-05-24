e651f6bf2716d879a0cf9b7d7fdffd5a405cb1e8
Joe Masilotti <joseph.masilotti@gmail.com>
2013-05-23 20:40:52 -0400

    Initial commit - barebones Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SauceApi::Application.config.secret_token = 'e4f3a7ca00fad0dfc5e45bdf2a239b67623a15ca6bfeeb3cc7563cb648f8a090b93b2281f54d0a67d9ad68ae3054e87cfafb81301a12c424045274c544931509'
