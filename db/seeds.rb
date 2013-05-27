f66b68f2987e789847fe023c586d344088f235c7
Joe Masilotti <joseph.masilotti@gmail.com>
2013-05-27 10:18:50 -0400

    Removed comma and special character from db seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-Sauce.create(:name => 'Cholula Hot Sauce', :manufacturer => 'Tequila Cuervo La Rojeña, S.A. de C.V..')
+Sauce.create(:name => 'Cholula Hot Sauce', :manufacturer => 'Tequila Cuervo La Rojena S.A. de C.V..')
 Sauce.create(:name => 'Tabasco Sauce', :manufacturer => 'McIlhenny Company')
