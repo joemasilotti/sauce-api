cd56e8c90e85f73bf7196244a1df3cedd96ae1f7
Joe Masilotti <joseph.masilotti@gmail.com>
2013-06-30 15:48:00 -0400

    Clean up Sauce spec

--- spec/models/sauce_spec.rb
+++ spec/models/sauce_spec.rb
@@ -2,18 +2,23 @@ require 'spec_helper'
 
 describe Sauce do
   describe "factories" do
-    it "should be valid for sauce" do
+    it "has a name" do
+      FactoryGirl.build(:sauce).name.should be_present
+    end
+
+    it "has a manufacturer" do
+      FactoryGirl.build(:sauce).manufacturer.should be_present
+    end
+
+    it "is valid" do
       FactoryGirl.create(:sauce).should be_valid
     end
   end
 
   describe "validations" do
-    it "require a name" do
-      FactoryGirl.build(:sauce, :name => "").should_not be_valid
-    end
-
-    it "require a manufacturer" do
-      FactoryGirl.build(:sauce, :manufacturer => nil).should_not be_valid
-    end
+    it { should belong_to(:manufacturer) }
+    it { should have_and_belong_to_many(:flavors) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:manufacturer) }
   end
 end
