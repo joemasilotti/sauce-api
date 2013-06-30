683587911afcc4d6362eb4c6264764d91e43708e
Joe Masilotti <joseph.masilotti@gmail.com>
2013-06-30 15:35:00 -0400

    Clean up Flavor spec

--- spec/models/flavor_spec.rb
+++ spec/models/flavor_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 describe Flavor do
   describe "factories" do
-    it "should be valid for flavor" do
+    it "has a name" do
+      FactoryGirl.build(:flavor).name.should be_present
+    end
+
+    it "is valid" do
       FactoryGirl.create(:flavor).should be_valid
     end
-  end
 
-  describe "validations" do
-    it "require a name" do
-      FactoryGirl.build(:flavor, :name => "").should_not be_valid
+    describe "validations" do
+      it { should validate_presence_of(:name) }
     end
   end
 end
