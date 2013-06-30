3ddca07df37487c7d960b3070e5934717c838378
Joe Masilotti <joseph.masilotti@gmail.com>
2013-06-30 15:29:08 -0400

    Clean up Admin model and spec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe Admin do
   describe "factories" do
-    it "should be valid for admin" do
+    it "has an email" do
+       FactoryGirl.build(:admin).email.should be_present
+    end
+
+    it "is valid" do
       FactoryGirl.create(:admin).should be_valid
     end
   end
